9d4bcf20c4221baed75ffaaca6893dd562f8e0df
MSeccan <merve742002@gmail.com>
2026-04-16 14:35:05 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter_windows_3.41.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\projects\\new flder\\pregnova-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter_windows_3.41.3-stable\\flutter"
+  "PROJECT_DIR=C:\\projects\\new flder\\pregnova-main"
+  "FLUTTER_ROOT=C:\\src\\flutter_windows_3.41.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\projects\\new flder\\pregnova-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\projects\\new flder\\pregnova-main"
+  "FLUTTER_TARGET=C:\\projects\\new flder\\pregnova-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuMw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NDhjMzJhZjAzNA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MzI3ZWQ4MTQ1MA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4x"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\projects\\new flder\\pregnova-main\\.dart_tool\\package_config.json"
+)
